8ef76ac0e0d9897b60bd0baa51b8c0656b39c1bd
hbprotoss <gamespy1991@gmail.com>
2016-06-27 13:45:42 +0800

    gson support

---
+++ src/main/java/io/hbprotoss/web/app/GsonHttpMessageConverterConfig.java
@@ -0,0 +1,28 @@
+package io.hbprotoss.web.app;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import io.hbprotoss.web.SpringfoxJsonToGsonAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import springfox.documentation.spring.web.json.Json;
+
+/**
+ * Created by hbprotoss on 6/27/16.
+ */
+@Configuration
+public class GsonHttpMessageConverterConfig {
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter() {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson());
+        return converter;
+    }
+
+    private Gson gson() {
+        final GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
+        return builder.create();
+    }
+}
